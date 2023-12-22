e05ffa3082e2a64831e92f735a7d9bc2662dae5c
mizan1235 <127872547+mizan1235@users.noreply.github.com>
2023-12-22 16:44:34 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -82,9 +82,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+        'OPTIONS': {
+            'deterministic': False,
+        },
     }
 }
+
 
 
 # Password validation
